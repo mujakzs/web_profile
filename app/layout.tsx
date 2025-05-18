763b98902e10182566196575473ea68d22b24a06
Andrian Gultiano <andriangultiano14@gmail.com>
2025-05-18 16:23:36 +0800

    ScrollToTop Functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Provider from "@/components/Hoc/Provider";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Rajdhani({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
           <ResponsiveNav />
           {children}
           <Footer/>
+          <ScrollToTop />
         </Provider>
         
       </body>
